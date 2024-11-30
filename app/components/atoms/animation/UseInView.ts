1bd604506160f4d7d7169fdf6264402ed03c4eeb
Fynwurk <fynwurk@gmail.com>
2024-11-30 18:38:27 +0530

    1st Deploy

---
+++ app/components/atoms/animation/UseInView.ts
@@ -0,0 +1,29 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+
+export const useInView = () => {
+  const [isInView, setIsInView] = useState(false);
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setIsInView(true);
+          }
+        });
+      },
+      { threshold: 0.1 } // Trigger when 10% of the element is visible
+    );
+
+    if (ref.current) observer.observe(ref.current);
+
+    return () => {
+      if (ref.current) observer.unobserve(ref.current);
+    };
+  }, []);
+
+  return { ref, isInView };
+};
